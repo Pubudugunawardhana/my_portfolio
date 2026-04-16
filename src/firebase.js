68fee6427b41c233d1bc45a83c4a78834effee59
pubudugunawardhana23 <pubudugunawardhana23@gmail.com>
2026-04-16 23:49:13 +0530

    Connect Firebase to Project

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,24 @@ import { getFirestore } from "firebase/firestore";
 // Your Firebase configuration object
 // NOTE: You must replace these placeholder values with the exact keys 
 // from your actual Google Firebase Console dashboard (Project Settings)!
+
+// const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+//   projectId: "YOUR_PROJECT_ID",
+//   storageBucket: "YOUR_PROJECT_ID.appspot.com",
+//   messagingSenderId: "YOUR_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA1A3OLToXhOWQQdxxxVMVM5V8OrmPObXQ",
+  authDomain: "my-portfolio-89169.firebaseapp.com",
+  projectId: "my-portfolio-89169",
+  storageBucket: "my-portfolio-89169.firebasestorage.app",
+  messagingSenderId: "515991294284",
+  appId: "1:515991294284:web:fbc4b8d124415cf3e27fe9",
+  measurementId: "G-87YVCMCC0Q"
 };
 
 // Initialize Firebase with your secure config
